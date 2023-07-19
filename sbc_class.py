2030cd45b1a77c3a9e09f02e12b17a0fa3b26709
thunguyen <tqn1472@gmail.com>
2023-07-19 16:56:43 -0700

    sbc classes edit

--- sbc_class.py
+++ sbc_class.py
@@ -2,54 +2,72 @@ import numpy as np
 import pandas as pd
 
 class Result: 
-    def __init__(self, name, p, v, use_sba, use_croston):
-        self.name = name 
+    def __init__(self, p, v, model_use):
         self.p = p 
         self.v = v 
-        self.use_croston = use_croston 
-        self.use_sba = use_sba
+        self.model_use = model_use
     def getDemandInterval(self): 
         return self.p 
     def getCVSquared(self): 
         return self.v
     def getCoefficients(self):
-        return f'p (demand interval), CV squared (coefficient of variation): {[self.p, self.v]}'
-    def getModelUsed(self): 
-        if self.use_sba: 
-            return "SBA"
-        if self.use_croston: 
-            return "Croston" 
+        print(f'p (demand interval), CV squared (coefficient of variation): {[self.p, self.v]}')
+        return [self.p, self.v]
+    def getModelUse(self): 
+        return self.model_use
         
 
-def classify_series(data, col_name, type ="SBC", plot = True): 
+def classify_series_helper(data, type ="SBC", plot = False): 
     # check if input data is an array/list, etc: 
-    if np.ndim(data) == 1 and not isinstance(data, pd.DataFrame): 
-        data = np.array(data)
-        data = data.reshape(len(data), -1)
-    elif isinstance(data, pd.DataFrame): 
-        try: 
-            data = np.array(data[col_name])
-        except: 
-            print("Please check column name")
-    else: 
-        print("please pass in a list or array")
-
+    data = data[~np.isnan(data)]
     nzd = np.where(data != 0)[0]
     k = len(nzd)
     z = data[nzd]
-    x = np.diff(nzd, prepend=0)
+    x = np.diff(nzd, prepend=-1)
     p = np.mean(x)
-    v = (np.std(z) / np.mean(z)) ** 2
+    v = (np.std(z, ddof=1) / np.mean(z)) ** 2
 
-    use_sba = False
-    use_croston = False
     if type == 'SBC': 
         if p > 1.32 or v < 0.49: 
-            use_sba = True 
+            model_use = 'SBA'
         else: 
-            use_croston = True
+            model_use = 'Croston'
     else: 
         print("Unsupported classification type")
 
-    return Result(col_name, p, v, use_sba, use_croston)
+    return Result(p, v, model_use)
+
+def sbc_class(data, type = 'SBC', plot=False):
+    # check if input data is an array/list, etc: 
+    if np.ndim(data) == 1 and not isinstance(data, pd.DataFrame): 
+        target = np.array(data)
+        target = target.reshape(len(target), -1)
+        res = classify_series_helper(target, type, plot)
+        print(f"Model use: {res.getModelUse()}")
+        res.getCoefficients()
+    elif np.ndim(data) == 1 and isinstance(data, pd.DataFrame): 
+        # assume the first column to be the target
+        target = np.array(data.iloc[:, 0])
+        res = classify_series_helper(target, type, plot)
+        print(f"Model use: {res.getModelUse()}")
+        res.getCoefficients()
+    elif np.ndim(data) >1 and isinstance(data, pd.DataFrame): 
+        target = data.to_numpy().T
+        p = [] 
+        v = [] 
+        model_use = [] 
+        out_df = pd.DataFrame()
+        for i in range(target.shape[0]): 
+            res = classify_series_helper(target[i], type, plot)
+            p.append(res.getDemandInterval())
+            v.append(res.getCVSquared())
+            model_use.append(res.getModelUse())
+        out_df['target'] = data.columns.to_list()
+        out_df['p'] = p 
+        out_df['CV squared'] = v 
+        out_df['model'] = model_use
+        return out_df
+    else: 
+        raise ValueError('Please pass in a list/array (for 1 target) or a dataframe (multiple targets)')
+    return (res.getModelUse(), [res.getDemandInterval(), res.getCVSquared()])
 
